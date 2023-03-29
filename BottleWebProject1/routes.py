1dc33c8263aeae8e273498b8eeaeb67d15764406
Manfred246 <manfredkun@yandex.ru>
2023-03-29 12:18:56 +0300

    fix bags

---
+++ BottleWebProject1/routes.py
@@ -0,0 +1,25 @@
+"""
+Routes and views for the bottle application.
+"""
+
+from bottle import route, view
+from datetime import datetime
+
+@route('/')
+@route('/home')
+@view('index')
+def home():
+    """Renders the home page."""
+    return dict(
+        year=datetime.now().year
+    )
+
+@route('/about')
+@view('about')
+def about():
+    """Renders the about page."""
+    return dict(
+        title='About',
+        message='Your application description page.',
+        year=datetime.now().year
+    )
--- BottleWeb_MK/views/about.tpl
+++
@@ -1,6 +0,0 @@
-﻿% rebase('layout.tpl', title=title, year=year)
-
-<h2>{{ title }}.</h2>
-<h3>{{ message }}</h3>
-
-<p>Use this area to provide additional information.</p>
